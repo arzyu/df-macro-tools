71e7805cd3683aa1d6a7916f7336f5b1eaffc2ae
arzyu <arzyu@live.cn>
2018-11-03 16:04:42 +0800

    update code style

--- df-macro.sh
+++ df-macro.sh
@@ -1,68 +1,68 @@
 #!/bin/bash
 
 function die() {
-  echo "$@" >&2; exit 1
+	echo "$@" >&2; exit 1
 }
 
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    -d|--destination)
-      destination="$2"
-      shift
-      ;;
-    *)
-      macro_file="$1"
-      ;;
-  esac
+while [[ $# > 0 ]]; do
+	case $1 in
+		-d|--destination)
+			destination="$2"
+			shift
+			;;
+		*)
+			macro_file="$1"
+			;;
+	esac
 
-  shift
+	shift
 done
 
 if [[ -z $macro_file ]]; then
-  die "No input file *.macro"
+	die "No input file *.macro"
 fi
 
 if [[ -z $destination ]]; then
-  destination="$(pwd)"
+	destination="$(pwd)"
 fi
 
 output_file_name="$(basename $macro_file .macro).mak"
 mak_file_base="$(dirname $macro_file)"
 
 function process_mak_file() {
-  mak_file=$1
-  sed -e "1 d;$ d" "$mak_file_base/$mak_file"
+	mak_file=$1
+	sed -e "1 d;$ d" "$mak_file_base/$mak_file"
 }
 
 function process_macro_line() {
-  words=($1)
-  case ${words[0]} in
-    mak)
-      process_mak_file "${1:4}"
-      ;;
-    *)
-      printf "\t\t$1\n\tEnd of group\n"
-      ;;
-  esac
+	words=($1)
+	case ${words[0]} in
+		mak)
+			process_mak_file "${1:4}"
+			;;
+		*)
+			printf "\t\t$1\n\tEnd of group\n"
+			;;
+	esac
 }
 
 function process_macro_file() {
-  macro_file=$1
+	macro_file=$1
 
-  while IFS= read -r line; do
-    # ignore blank lines and comments
-    if [[ -z $line || ${line:0:1} == "#" ]]; then
-      continue
-    fi
+	while IFS= read -r line; do
+		# ignore blank lines and comments
+		if [[ -z $line || ${line:0:1} == "#" ]]; then
+			continue
+		fi
 
-    process_macro_line "$line"
-  done < "$macro_file"
+		process_macro_line "$line"
+	done < "$macro_file"
 }
 
-cat > "$destination/$output_file_name" << EOF
-$(basename $output_file_name .mak)
-$(process_macro_file $macro_file)
-End of macro
+cat > "$destination/$output_file_name" <<- EOF
+	$(basename $output_file_name .mak)
+	$(process_macro_file $macro_file)
+	End of macro
 EOF
 
 printf " => output: $destination/$output_file_name\n"
